7982e0d813b16b1cb32d07e035b269c36392f360
Daniel Mueller <daniel@danm.de>
2012-05-06 20:43:52 +0200

    Added proper version and updated Changelog + THANKS

--- pcsc-sharp/Properties/AssemblyInfo.cs
+++ pcsc-sharp/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("8f2ed04d-e1d6-446d-a99f-357a366c33f4")]
 
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyVersion("2.0.0.4")]
+[assembly: AssemblyFileVersion("2.0.0.4")]
