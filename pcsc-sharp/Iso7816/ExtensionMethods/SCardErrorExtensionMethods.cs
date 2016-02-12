7c2bad382b3bebf6c16c01d8699708de09309b8a
Daniel Mueller <daniel@danm.de>
2016-02-12 20:34:45 +0100

    IsoReader: InsufficientBuffer - accept Windows ERROR_INSUFFICIENT_BUFFER
    - HasInsufficientBuffer tests for Windows

--- pcsc-sharp/Iso7816/ExtensionMethods/SCardErrorExtensionMethods.cs
+++ pcsc-sharp/Iso7816/ExtensionMethods/SCardErrorExtensionMethods.cs
@@ -1,9 +1,12 @@
-﻿namespace PCSC.Iso7816
+﻿using PCSC.Interop;
+
+namespace PCSC.Iso7816
 {
     internal static class SCardErrorExtensionMethods
     {
         public static bool HasInsufficientBuffer(this SCardError sc) {
-            return sc == SCardError.InsufficientBuffer;
+            return sc == SCardError.InsufficientBuffer
+                   || (Platform.IsWindows && sc == SCardError.WinErrorInsufficientBuffer);
         }
     }
 }
