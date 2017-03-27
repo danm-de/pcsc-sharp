b9f03f3835508f4e2ead30879ca936bd1156494b
Mark <mark@gototags.com>
2017-03-27 12:11:51 -0700

    Fix SCardState enum

--- pcsc-sharp/Enums/SCardState.cs
+++ pcsc-sharp/Enums/SCardState.cs
@@ -4,30 +4,28 @@ using System.ComponentModel;
 namespace PCSC
 {
     /// <summary>State of the smart card in the reader.</summary>
-    /// <remarks>Is a bit mask that represents the current reader status.</remarks>
-    [Flags]
     public enum SCardState
     {
         /// <summary>Unknown status.</summary>
         [Description("Unknown state")]
-        Unknown = 0x0001,
+        Unknown = 0x00,
         /// <summary>There is no card in the reader.</summary>
         [Description("Card is absent")]
-        Absent = 0x0002,
+        Absent = 0x01,
         /// <summary>There is a card in the reader, but it has not been moved into position for use.</summary>
         [Description("Card is present")]
-        Present = 0x0004,
+        Present = 0x02,
         /// <summary>There is a card in the reader in position for use. The card is not powered.</summary>
         [Description("Card not powered")]
-        Swallowed = 0x0008,
+        Swallowed = 0x03,
         /// <summary>Power is being provided to the card, but the reader driver is unaware of the mode of the card.</summary>
         [Description("Card is powered")]
-        Powered = 0x0010,
+        Powered = 0x04,
         /// <summary>The card has been reset and is awaiting PTS negotiation.</summary>
         [Description("Ready for PTS")]
-        Negotiable = 0x0020,
+        Negotiable = 0x05,
         /// <summary>The card has been reset and specific communication protocols have been established.</summary>
         [Description("PTS has been set")]
-        Specific = 0x0040
+        Specific = 0x06
     }
 }
