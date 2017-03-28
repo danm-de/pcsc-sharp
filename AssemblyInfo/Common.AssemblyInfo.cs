6391a04e0306e356e1b1e47132cdb9a976da8751
Daniel Mueller <daniel@danm.de>
2017-03-28 20:18:10 +0200

    Prepare bugfix release 3.6.1
    - new version 3.6.1, for bugfix
    https://github.com/danm-de/pcsc-sharp/issues/44.

--- AssemblyInfo/Common.AssemblyInfo.cs
+++ AssemblyInfo/Common.AssemblyInfo.cs
@@ -4,8 +4,8 @@
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright © 2007-2016 by Daniel Mueller")]
+[assembly: AssemblyCopyright("Copyright © 2007-2017 by Daniel Mueller")]
 
-[assembly: AssemblyVersion("3.6.0")]
-[assembly: AssemblyFileVersion("3.6.0")]
-[assembly: AssemblyInformationalVersion("3.6.0")]
+[assembly: AssemblyVersion("3.6.1")]
+[assembly: AssemblyFileVersion("3.6.1")]
+[assembly: AssemblyInformationalVersion("3.6.1")]
