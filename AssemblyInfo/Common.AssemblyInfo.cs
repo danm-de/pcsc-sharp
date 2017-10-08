73c1a88db7da9e451cfd4c207db22e9c4c77cc70
Daniel Mueller <daniel@danm.de>
2017-10-08 16:54:15 +0200

    New version 3.8.0
    - code cleanups
    - new public constructor in Iso7816.Response

--- AssemblyInfo/Common.AssemblyInfo.cs
+++ AssemblyInfo/Common.AssemblyInfo.cs
@@ -6,6 +6,6 @@
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("Copyright © 2007-2017 by Daniel Mueller")]
 
-[assembly: AssemblyVersion("3.7.0")]
-[assembly: AssemblyFileVersion("3.7.0")]
-[assembly: AssemblyInformationalVersion("3.7.0")]
+[assembly: AssemblyVersion("3.8.0")]
+[assembly: AssemblyFileVersion("3.8.0")]
+[assembly: AssemblyInformationalVersion("3.8.0")]
