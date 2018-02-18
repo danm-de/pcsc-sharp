3873bc1942330c61764bf1616c68559ec57c824d
Daniel Mueller <daniel@danm.de>
2018-02-18 18:14:56 +0100

    Removing old .Net Framework 4.0 csproj file, Added .netstandard project file

--- pcsc-sharp/Properties/AssemblyInfo.cs
+++ pcsc-sharp/Properties/AssemblyInfo.cs
@@ -1,9 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("PC/SC Sharp")]
-[assembly: AssemblyDescription("PC/SC wrapper classes for .NET")]
-[assembly: AssemblyProduct("PC/SC Sharp")]
+﻿using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: Guid("8f2ed04d-e1d6-446d-a99f-357a366c33f4")]
