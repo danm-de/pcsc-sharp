346d6143a27dc922e17708ea01cfa5c20e1fa990
Daniel Mueller <daniel@danm.de>
2018-02-18 19:15:50 +0100

    Changed pcsc-sharp libraries to .netstandard 2.0 - removed old documentation (html files) - added editorconfig file - upgrade of several nuget packages (such as fluentassertions) - changed paket.exe to use magic mode - removed paket.template files (msbuild can produce nuget packages)

--- restore-packages.cmd
+++ restore-packages.cmd
@@ -4,4 +4,4 @@ set root=%~dp0
 set dir=%root%.paket
 set paket=%dir%\paket.exe
 
-"%paket%" %*
+"%paket%" restore
---
+++ src/PCSC.Reactive/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("9a5b987d-db36-4f8a-a62e-642deef51f7d")]
