d6e5fae3d3b4fd52f5f981f56d0df15d0fad5433
Daniel Mueller <daniel@danm.de>
2018-09-24 17:09:02 +0200

    Update to version 4.0.1

--- AssemblyInfo/Common.AssemblyInfo.cs
+++ AssemblyInfo/Common.AssemblyInfo.cs
@@ -4,8 +4,8 @@
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright © 2007-2017 by Daniel Mueller")]
+[assembly: AssemblyCopyright("Copyright © 2007-2018 by Daniel Mueller")]
 
-[assembly: AssemblyVersion("3.8.0")]
-[assembly: AssemblyFileVersion("3.8.0")]
-[assembly: AssemblyInformationalVersion("3.8.0")]
+[assembly: AssemblyVersion("4.0.1")]
+[assembly: AssemblyFileVersion("4.0.1")]
+[assembly: AssemblyInformationalVersion("4.0.1")]
