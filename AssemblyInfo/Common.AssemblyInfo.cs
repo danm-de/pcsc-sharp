5db195d74c7cb3a681c5257ab90cb73cba882888
Daniel Mueller <daniel@danm.de>
2019-02-18 22:55:03 +0100

    New version 4.0.3
    Bugfix release for #86

--- AssemblyInfo/Common.AssemblyInfo.cs
+++ AssemblyInfo/Common.AssemblyInfo.cs
@@ -6,6 +6,6 @@
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("Copyright © 2007-2019 by Daniel Mueller")]
 
-[assembly: AssemblyVersion("4.0.2")]
-[assembly: AssemblyFileVersion("4.0.2")]
-[assembly: AssemblyInformationalVersion("4.0.2")]
+[assembly: AssemblyVersion("4.0.3")]
+[assembly: AssemblyFileVersion("4.0.3")]
+[assembly: AssemblyInformationalVersion("4.0.3")]
