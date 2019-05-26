4ef3055d92d8b6b11474bce8915e4fb0c4bdf72c
Daniel Mueller <daniel@danm.de>
2019-05-26 02:51:30 +0200

    Added NuGetPush target to build script.

--- build.fsx
+++ build.fsx
@@ -1,6 +1,7 @@
 #load ".fake/build.fsx/intellisense.fsx"
 open Fake.Core
 open Fake.DotNet
+open Fake.DotNet.NuGet
 open Fake.IO
 open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
@@ -25,11 +26,29 @@ Target.create "Test" (fun _ ->
       |> Seq.iter (DotNet.test id)
 )
 
+Target.create "NuGetPush" (fun _ ->
+    let setNugetPushParams (defaults:NuGet.NuGetPushParams) =
+        { defaults with
+            Source = Some "https://api.nuget.org/v3/index.json"
+        }
+
+    let setParams (defaults:DotNet.NuGetPushOptions) =
+        { defaults with
+            PushParams = setNugetPushParams defaults.PushParams
+        }
+
+    !! "src/**/bin/Release/*.nupkg"
+      |> Seq.iter (fun file -> DotNet.nugetPush setParams file)
+)
+
 Target.create "All" ignore
 
 "Clean"
   ==> "Build"
   ==> "Test"
   ==> "All"
+
+"Build"
+  ==> "NuGetPush"
 
 Target.runOrDefault "All"
