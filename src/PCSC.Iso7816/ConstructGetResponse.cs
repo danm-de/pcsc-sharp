147f6012ba27d956bdae0fed6943caa7738caeef
Daniel Mueller <daniel@danm.de>
2019-12-07 20:39:19 +0100

    Fixes #97
    * Update NuGet packages: all test projects (NUnit, FakeItEasy, ...)
    * Update NuGet packages: System.Reactive >= 4.3.0
    * Added new build targets: netstandard2.1, netcoreapp3.0, netcoreapp3.1,
      net472, net48
    * PCSC.Iso7816: BREAKING CHANGE: if the card signals "more data available"
      (GET RESPONSE required), it copies the CLA byte from the previous command.
    * PCSC.Iso7816: new method Transmit(CommandApdu, ConstructGetResponse)
      Gives control on how to create a GET RESPONSE command.
    * Fixes https://github.com/danm-de/pcsc-sharp/issues/97
      Thanks to @gjermund-stensrud for reporting

---
+++ src/PCSC.Iso7816/ConstructGetResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace PCSC.Iso7816
+{
+    /// <summary>
+    /// Creates a GET RESPONSE command after receiving SW1=0x61 (More data available)
+    /// </summary>
+    /// <param name="initialCommand">The initial command that has been sent to the card</param>
+    /// <param name="previousResponse">The received response</param>
+    /// <param name="le">The expected size</param>
+    /// <returns>A GET RESPONSE APDU</returns>
+    public delegate CommandApdu ConstructGetResponse(
+        CommandApdu initialCommand,
+        ResponseApdu previousResponse,
+        int le);
+}
